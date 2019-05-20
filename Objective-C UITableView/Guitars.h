532345401dbec7a271b43d630e9fdabfee550d99
NP2 <sahandraeisi1994@gmail.com>
2019-05-20 12:14:35 +0430

    I have no idea what the hell.....

---
+++ Objective-C UITableView/Guitars.h
@@ -0,0 +1,20 @@
+//
+//  Guitars.h
+//  Objective-C UITableView
+//
+//  Created by NP2 on 5/20/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Guitars : NSObject
+
+@property (strong,nonatomic) NSString *name;
+@property (strong,nonatomic) NSString *numberOfStrings;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Objective-C UITableView/ViewController.m
+++ Objective-C UITableView/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "Guitars.h"
 
 @interface ViewController ()
+
+@property (strong ,nonatomic) NSMutableArray<Guitars *> *guitars;
 
 @end
 
@@ -16,20 +19,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self setGuitars];
+    
     self.navigationItem.title = @"Ibanez Guitars";
     self.navigationController.navigationBar.prefersLargeTitles = YES;
     
     [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:@"TableViewCell"];
 }
 
+- (void)setGuitars {
+    self.guitars = NSMutableArray.new;
+    
+    Guitars *guitars6Strings = Guitars.new;
+    guitars6Strings.name = @"rg2550e Team J. Craft";
+    guitars6Strings.numberOfStrings = @"6";
+    
+    [self.guitars addObject:guitars6Strings];
+    
+    Guitars *guitars7Strings = Guitars.new;
+    guitars7Strings.name = @"rg 2770 edge pro II";
+    guitars7Strings.numberOfStrings = @"7";
+    
+    [self.guitars addObject:guitars7Strings];
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
+    return self.guitars.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TableViewCell" forIndexPath:indexPath];
     
-    cell.textLabel.text = @"rg2550e 2007 Team J. Craft";
+    Guitars *guitar = self.guitars[indexPath.row];
+    
+    cell.textLabel.text = guitar.name;
     return cell;
 }
 
