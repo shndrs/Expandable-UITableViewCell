30e78c97e4f76b90640379a7261eb60935b88a71
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-03-11 10:04:47 +0330

    Update with DISPATCH_QUEUE_PRIORITY_BACKGROUND

--- objc-b/objc-b/ViewController.m
+++ objc-b/objc-b/ViewController.m
@@ -17,36 +17,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSString *productTitle = @"Ibanez Prestige";
-    NSString *manufacturer = @"Ibanez Incorporation";
-    NSString *longHand = [NSString stringWithUTF8String:"Ibanez Prestige"];
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
+    dispatch_async(queue, ^{
+        NSString *productTitle = @"Ibanez Prestige";
+        NSString *manufacturer = @"Ibanez Incorporation";
+        NSString *longHand = [NSString stringWithUTF8String:"Ibanez Prestige"];
+       
+        char model[] = "2550e Team J. Craft";
+       
+        int stringsNumber = 6;
+        int quantity = 134;
+        float price = 1099.4;
+       
+        float unitPrice = price/quantity;
+       
+        NSNumber *unitPriceObject = [NSNumber numberWithFloat:unitPrice];
+       
+        NSArray *priceArray = @[@(unitPrice)];
+       
+        float test = [unitPriceObject floatValue] + 5;
+       
+        NSString *blurb = [NSString stringWithFormat:@"You have selected %@ %s,\r\n made by %@ it's a %i strings guitar and the price is: %.2f USD", productTitle, model, manufacturer, stringsNumber, price];
+        NSLog(@"%@", blurb);
+       
+        // MARK: - Collection Types
+       
+        NSDictionary *bostonDict = @{@"City":@"Boston", @"Country":@"USA"};
+       
+        NSMutableDictionary *mutBostonDict = [[NSMutableDictionary alloc] initWithDictionary: bostonDict];
+       
+        NSString *beantown = [bostonDict valueForKey:@"City"];
+        NSLog(@"%@", beantown);
+        [mutBostonDict setValue:@"Italy" forKey:@"Country"];
+        [mutBostonDict setValue:@"Milan" forKey:@"City"];
+       
+        NSMutableDictionary *guitarDict = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"Ibanez", @"Brand", @"rg2550e Team J. Craft", @"Model", nil];
+       
+        [guitarDict setValue:@"Ormsby" forKey:@"Brand"];
+        [guitarDict setValue:@"HypeGRT" forKey:@"Model"];
+        [guitarDict addEntriesFromDictionary:@{@"NumberOfStrings":@(7)}];
+       
+        NSArray *shitArray = @[mutBostonDict, guitarDict];
+       
+        NSLog(@"%@", shitArray);
+    });
     
-    char model[] = "2550e Team J. Craft";
     
-    int stringsNumber = 6;
-    int quantity = 134;
-    float price = 1099.4;
-    
-    float unitPrice = price/quantity;
-    
-    NSNumber *unitPriceObject = [NSNumber numberWithFloat:unitPrice];
-    
-    NSArray *priceArray = @[@(unitPrice)];
-    
-    float test = [unitPriceObject floatValue] + 5;
-    
-    NSString *blurb = [NSString stringWithFormat:@"You have selected %@ %s,\r\n made by %@ it's a %i strings guitar and the price is: %.2f USD", productTitle, model, manufacturer, stringsNumber, price];
-    NSLog(@"%@", blurb);
-    
-    // MARK: - Collection Types
-    
-    NSDictionary *bostonDict = @{@"City":@"Boston", @"Country":@"USA"};
-    
-    NSMutableDictionary *mutBostonDict = [[NSMutableDictionary alloc] initWithDictionary: bostonDict];
-    
-    NSString *beantown = [bostonDict valueForKey:@"City"];
-    [mutBostonDict setValue:@"Italy" forKey:@"Country"];
-    [mutBostonDict setValue:@"Milan" forKey:@"City"];
+   
         
 }
 
